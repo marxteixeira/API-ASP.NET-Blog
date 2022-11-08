3b1dd791fc33ef96f69252655eb57a72ec2d5ae2
Marx Teixeira <95075808+marxteixeira@users.noreply.github.com>
2022-11-08 20:19:39 -0300

    criado o service TokenService

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,26 @@
-﻿namespace Blog.Services
+﻿using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Blog.Services
 {
     public class TokenService
     {
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            var tokenDescriptor = new SecurityTokenDescriptor 
+            {
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials
+                (
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
