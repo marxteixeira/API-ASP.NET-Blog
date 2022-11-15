8cd28a6f928d05d94e3063af65aa001c99afedab
Marx Teixeira <95075808+marxteixeira@users.noreply.github.com>
2022-11-14 23:02:55 -0300

    finalizando ApiKey para que robôs possam fazer requisição.

---
+++ Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Blog.Attributes
+{
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Query.TryGetValue(Configuration.ApiKeyName, out var extractApiKey))
+                {
+                    context.Result = new ContentResult()
+                    {
+                        StatusCode = 401,
+                        Content = "ApiKey não encontrada"
+                    };
+                return; 
+                };
+
+            if (!Configuration.ApiKey.Equals(extractApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 403,
+                    Content = "Acesso não autorizado."
+                };
+                return ;
+            }
+
+            await next();
+
+
+        }
+
+        
+    }
+}
--- Configuration.cs
+++ Configuration.cs
@@ -3,5 +3,7 @@
     public static class Configuration
     {
         public static string JwtKey { get; set; } = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+        public static string ApiKeyName { get; set; } = "api_key";
+        public static string ApiKey { get; set; } = "curso_api_IlTevUM/z0ey3NCV/unWg==";
     }
 }
