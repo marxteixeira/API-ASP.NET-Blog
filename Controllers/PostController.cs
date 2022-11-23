d9682ac95fcf18f40978d1a4d1c81d4845ea6445
marxteixeira <marxtts@gmail.com>
2022-11-23 15:25:36 -0300

    criacao de DTO para retorno dos Posts

---
+++ Controllers/PostController.cs
@@ -0,0 +1,34 @@
+﻿using Blog.Data;
+using Blog.ViewModel.Posts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet("v1/posts")]
+        public async Task<IActionResult> GetAsync(
+            [FromServices] BlogDataContext context)
+        {
+            var posts = await context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Include(x => x.Author)
+                .Select(x =>
+                new ListPostsViewModels
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    LastUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})"
+                })
+                .ToListAsync();  
+            return Ok(posts);
+        }
+    }
+}
---
+++ ViewModel/Posts/ListPostsViewModels.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.ViewModel.Posts
+{
+    public class ListPostsViewModels
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+    }
+}
