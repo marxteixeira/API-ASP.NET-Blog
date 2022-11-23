2da96085821830c78ec6e5ef01fe5873f8040853
Marx Teixeira <95075808+marxteixeira@users.noreply.github.com>
2022-11-23 18:25:02 -0300

    adicionado metodo para busca por categorias

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -78,5 +78,50 @@ namespace Blog.Controllers
                 return StatusCode(500, new ResultViewModel<List<Post>>("05X04 - Falha interna no servidor"));
             }
         }
+
+        [HttpGet("v1/posts/category/{category}")]
+        public async Task<IActionResult> GetByCategoryAsync(
+            [FromRoute] string category,
+            [FromServices] BlogDataContext context,
+            [FromQuery] int page = 0,
+            [FromQuery] int pageSize = 25)
+        {
+            try
+            {
+                var count = await context.Posts.AsNoTracking().CountAsync();
+                var posts = await context
+                    .Posts
+                    .AsNoTracking()
+                    .Include(x => x.Author)
+                    .Include(x => x.Category)
+                    .Where(x => x.Category.Slug == category)    
+                    .Select(x =>
+                    new ListPostsViewModels
+                    {
+                        Id = x.Id,
+                        Title = x.Title,
+                        Slug = x.Slug,
+                        LastUpdateDate = x.LastUpdateDate,
+                        Category = x.Category.Name,
+                        Author = $"{x.Author.Name} ({x.Author.Email})"
+                    })
+                    .Skip(page * pageSize)
+                    .Take(pageSize)
+                    .OrderByDescending(x => x.LastUpdateDate)
+                    .ToListAsync();
+
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    total = count,
+                    page,
+                    pageSize,
+                    posts
+                }));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<List<Post>>("05X04 - Falha interna no servidor"));
+            }
+        }
     }
 }
