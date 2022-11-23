81992d86fbec6ec57f8e3b0e2057addc0c28eee4
marxteixeira <marxtts@gmail.com>
2022-11-23 16:47:41 -0300

    finalizado paginação de dados

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -10,7 +10,9 @@ namespace Blog.Controllers
     {
         [HttpGet("v1/posts")]
         public async Task<IActionResult> GetAsync(
-            [FromServices] BlogDataContext context)
+            [FromServices] BlogDataContext context,
+            [FromQuery] int page = 0,
+            [FromQuery] int pageSize = 25)
         {
             var posts = await context
                 .Posts
@@ -27,6 +29,9 @@ namespace Blog.Controllers
                     Category = x.Category.Name,
                     Author = $"{x.Author.Name} ({x.Author.Email})"
                 })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.LastUpdateDate)
                 .ToListAsync();  
             return Ok(posts);
         }
